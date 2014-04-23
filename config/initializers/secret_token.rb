54a586d7fb2de13654b0c0c45f7955fe219af7af
Deltron <jamiedbuell@gmail.com>
2014-04-22 17:00:54 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Archer2::Application.config.secret_token = '4ebd0458350a5a11957d931b19063f56211d727cf3d6f19eae2e5e16d3a9343c20625758365e5a7bfcbc6d8f9af07e3d433d4e46bd1999d68c3284b096cdf793'
