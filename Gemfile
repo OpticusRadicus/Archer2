8032d8db8b7da70749d6634d86dd8734d71968da
Deltron <jamiedbuell@gmail.com>
2014-04-26 13:32:14 -0700

    add css some rpbs still

--- Gemfile
+++ Gemfile
@@ -1,12 +1,14 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
+gem 'bootstrap-sass'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 # gem 'sqlite3'
 gem 'pg'
+gem 'factory_girl_rails'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Archer2::Application.routes.draw do
-  get "static_pages/home"
+  # get "static_pages/home"
 
-  get "static_pages/help"
+
+match '/help', to: 'static_pages#help'
+
+match '/about', to: 'static_pages#about'
+
+match '/contact', to: 'static_pages#contact'
+
 
   resources :users
 
@@ -54,7 +60,7 @@ Archer2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
